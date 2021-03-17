534694d100db15f6d519df78b2d0bdf45747c870
zhuxiaomei <865622793@qq.com>
2021-03-17 21:04:47 +0800

    move some function

--- ui/ui_helper.py
+++ ui/ui_helper.py
@@ -1,7 +1,8 @@
 from PIL import ImageTk, Image
 import cv2
 import tkinter as tk
-from function import choose_file
+from tkinter import filedialog
+import os
 
 
 def get_max_window():
@@ -23,6 +24,35 @@ def show_img(cv_img, canvas):
 def get_choose_file_button(window, btn_text, string_var=None):
     button = tk.Button(window, text=btn_text, command=lambda: choose_file(string_var))  # 可以用lambda把var传进去
     return button
+
+
+def rotate_video():
+    # TODO:
+    pass
+
+
+def start_detect():
+    pass
+
+
+def choose_file(string_var=None):
+    """:returns file_path"""
+    file_path = filedialog.askopenfilename()
+
+    if string_var is not None:
+        string_var.set(file_path)
+
+    return file_path
+
+
+def split_url(url):
+    (_dir, file) = os.path.split(url)
+    temp = file.split('.')
+    filename = temp[0]
+    ext = temp[1]
+    result = {'dir': _dir, 'filename': filename, 'ext': ext}
+    print(result)
+    return result
 
 
 if __name__ == '__main__':
