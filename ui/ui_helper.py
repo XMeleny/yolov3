bd3e84b1e39cb14778f1b54a64a172f245a9b880
zhuxiaomei <865622793@qq.com>
2021-03-21 22:21:06 +0800

    optimize ui

--- ui/ui_helper.py
+++ ui/ui_helper.py
@@ -68,7 +68,13 @@ def get_choose_file_button_and_label(window):
     return button, label
 
 
+def rotate_image():
+    # 可以用个hash map把4个角度的图片存下来，避免多次计算。
+    pass
+
+
 def rotate_video():
+    # 获取旋转角度
     pass
 
 
@@ -105,43 +111,69 @@ def log():
     pass
 
 
+def update_process():
+    pass
+
+
 def init_window():
     # TODO: 用变量把text们都存起来
     # TODO: 修改颜色
     # TODO: 按钮可用状态转换
     window = get_max_window()
 
-    file_path_label_weight = 10
+    file_path_label_weight = 10  # 表示路径标签的宽度横跨多少列
 
+    choose_video_text = '选择视频'
+    video_path_text = '视频位置...'
+    rotate_video_text = '旋转视频'
+    model_path_text = '模型位置...'
+    choose_model_text = '选择模型'
+    choose_classes_text = '选择检测类别'
+    start_detect_text = '开始检测'
+    progress_text = '显示进度...'
+
+    label_relief = 'ridge'
+
+    # 第一行，视频第一帧显示
     canvas = tk.Canvas(window, bg='green',
                        width=window.winfo_width(),
                        height=window.winfo_height() - 200)
     canvas.grid(row=0, columnspan=file_path_label_weight + 3)
 
+    # 第二行，选择视频按钮、视频位置标签、旋转视频按钮
     choose_video_button, video_path_label = get_choose_file_button_and_label(window)
-    choose_video_button['text'] = "选择视频"
+    rotate_video_button = tk.Button(window, text=rotate_video_text, command=rotate_video)
+
+    choose_video_button['text'] = choose_video_text
+    video_path_label['text'] = video_path_text
+    video_path_label['relief'] = label_relief
     video_path_label['fg'] = 'gray'
-    video_path_label['bg'] = 'red'
-    rotate_video_button = tk.Button(window, text='旋转视频', command=rotate_video)
+    video_path_label['anchor'] = 'w'
 
     choose_video_button.grid(row=1, column=0, sticky='we')
     video_path_label.grid(row=1, column=1, columnspan=file_path_label_weight, sticky='we')
     rotate_video_button.grid(row=1, column=file_path_label_weight + 1, sticky='we')
 
+    # 第三行，选择模型按钮、模型位置标签、选择检测类别按钮
     choose_model_button, model_path_label = get_choose_file_button_and_label(window)
-    choose_model_button['text'] = "选择模型"
+    choose_classes_button = tk.Button(window, text=choose_classes_text)  # TODO: command
+
+    choose_model_button['text'] = choose_model_text
+    model_path_label['text'] = model_path_text
+    model_path_label['relief'] = label_relief
     model_path_label['fg'] = 'gray'
-    model_path_label['bg'] = 'red'
-    choose_classes_button = tk.Button(window, text="选择检测类别")  # TODO: command
+    model_path_label['anchor'] = 'w'
 
     choose_model_button.grid(row=2, column=0, sticky='we')
     model_path_label.grid(row=2, column=1, columnspan=file_path_label_weight, sticky='we')
     choose_classes_button.grid(row=2, column=file_path_label_weight + 1, sticky='we')
 
-    start_detect_button = tk.Button(window, text="开始检测")  # TODO: command
+    # 跨二三行，检测按钮
+    start_detect_button = tk.Button(window, text=start_detect_text)  # TODO: command
     start_detect_button.grid(row=1, column=2 + file_path_label_weight, rowspan=2, sticky='wens')
 
-    process_label = tk.Label(window, text='进度...', fg='gray', bg='#000000')
+    # 第四行，进度标签
+    process_label = tk.Label(window, fg='gray', text=progress_text, anchor='w', relief=label_relief)
     process_label.grid(row=3, columnspan=3 + file_path_label_weight, sticky='we')
 
     window.mainloop()
